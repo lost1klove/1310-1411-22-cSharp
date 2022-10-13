fc91d1a19c638e3bb50db9084640d108f37c1e53
lost1k <only.lost1k@gmail.com>
2022-10-14 02:02:53 +0300

    added hometask08

---
+++ HomeTask08/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая 
+// (1)на вход принимает число (N), а 
+// (2)на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+if (number > 0 )
+
+{
+    while (number >= count)
+    {
+        Console.Write($"{count} ");
+        count = count + 2;
+    }
+}
+else
+{
+    Console.WriteLine("Введите положительное целое число!");
+}
