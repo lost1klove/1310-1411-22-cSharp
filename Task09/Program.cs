6060d2c4e4d1e32b104bb59a40f7525be83a180b
lost1k <only.lost1k@gmail.com>
2022-10-18 01:13:58 +0300

    edit task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -6,16 +6,27 @@
 // 85 -> 8
 
 int number = new Random().Next(10, 100);
-Console.WriteLine($"Ваше случайное число : {number}") ;
+// Console.WriteLine($"Ваше случайное число : {number}") ;
 
 int firstDigit = number / 10; // 78 / 10 = 7
 int secondDigit = number % 10;
 
-if (firstDigit > secondDigit)
-{
-    Console.WriteLine($"Наибольшая цифра числа : {firstDigit}");
-}
-else
-{
-    Console.WriteLine($"Наибольшая цифра числа : {secondDigit}");
-}
+// if (firstDigit > secondDigit)
+// {
+//     Console.WriteLine($"Наибольшая цифра числа : {firstDigit}");
+// }
+// else
+// {
+//     Console.WriteLine($"Наибольшая цифра числа : {secondDigit}");
+// }
+
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Наибольшая цифра числа {number} равна: " + maxDigit);
+
+
+
+
+
+
+
+
