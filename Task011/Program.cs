1f1e1c30ea3a9803b7a601b1df4666f0b4bbfc69
lost1k <only.lost1k@gmail.com>
2022-10-18 02:17:49 +0300

    add task11

---
+++ Task011/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 11. Напишите программу, которая 
+// 1. выводит случайное трёхзначное число и 
+// 2. удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Ваше случайное число : {number}");
+
+int DelSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    return (firstDigit*10)+thirdDigit;
+}
+
+
+int result = DelSecondDigit(number);
+
+Console.WriteLine($"Полученное число : {result}");
+
