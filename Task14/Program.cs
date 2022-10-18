0528bd581f95724476342c0ef7e3837aac64823f
lost1k <only.lost1k@gmail.com>
2022-10-18 03:05:03 +0300

    edit task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -5,11 +5,33 @@
 // 46 -> нет
 // 161 -> да
 
+
+
+// Console.WriteLine("Введите число : ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+
+
+// if (numberA % 7 == 0 && numberA % 23 == 0)
+//     Console.WriteLine("Кратно");
+// else
+//     Console.WriteLine("Не кратно");
+
+
+bool DivNum(int digit, int num1, int num2)
+{
+    int result1 = digit % num1;
+    int result2 = digit % num2;
+    if (result1 == 0 && result2 == 0) return true;
+    return false;
+}
+
 Console.WriteLine("Введите число : ");
-int numberA = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = 7, number2 = 23;
 
+bool result = DivNum(number, number1, number2);
 
-if (numberA % 7 == 0 && numberA % 23 == 0)
+if (result)
     Console.WriteLine("Кратно");
 else
-    Console.WriteLine("Не кратно");
+    Console.WriteLine($"Не кратно");
---
+++ Task16/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 16. Напишите программу, которая 
+// 1. принимает на вход два числа и 
+// 2. проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Введите число А : ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите число В : ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+
+if (numberA == (numberB * numberB) && numberB/numberA==0)
+{
+    Console.Write($"число {numberA} является квадратом числа {numberB}");
+}
+// else if (numberB == (numberA * numberA))
+// {
+//     Console.Write($"число {numberB} является квадратом числа {numberA}");
+// }
