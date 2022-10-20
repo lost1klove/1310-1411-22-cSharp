492a5d679c110056c6df81c078369b2eb1715a49
lost1k <only.lost1k@gmail.com>
2022-10-20 12:31:06 +0300

    add task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 22: Напишите программу, которая
+// 1. принимает на вход число (N) и 
+// 2. выдаёт таблицу квадратов чисел от 1 до N.
+
+void Sqrt(int num)
+{
+    int count = 1;
+    if (num > 0)
+        while (num >= count)
+        {
+            Console.WriteLine($"{count,4} -> {count * count,4}");
+            count++;
+        }
+    else
+    {
+        Console.WriteLine("Введите натуральное число!");
+    }
+}
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Sqrt(number);
