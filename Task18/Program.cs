8465a8dd16fee1bafa71b3dd80ea3df68d142d4f
lost1k <only.lost1k@gmail.com>
2022-10-20 11:27:18 +0300

    add task18

---
+++ Task18/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 18: Напишите программу, которая по
+// 1. заданному номеру четверти, 
+// 2. показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите четверть ,в которой расположена заданная точка :");
+string position = Console.ReadLine();
+
+string Position(string pos)
+{
+    if (pos == "1") return "x > 0, y > 0";
+    if (pos == "2") return "x > 0, y < 0 ";
+    if (pos == "3") return "x < 0, y < 0";
+    if (pos == "4") return "x < 0, y > 0";
+    return "Введено некорректное значение";
+}
+
+Console.WriteLine(Position(position));
