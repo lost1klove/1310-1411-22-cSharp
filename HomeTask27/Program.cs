f514605a0d5a791d9f1c972b5a0f29d1f011efa4
lost1k <only.lost1k@gmail.com>
2022-11-10 03:51:17 +0300

    add HT27

--- HomeTask27/Program.cs
+++ HomeTask27/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. выдаёт сумму цифр в числе.
+
+
+Console.WriteLine("Введите число : ");
+long num = Convert.ToInt64(Console.ReadLine());
+ 
+ long Summ(long number)
+ {
+    long summary = 0;
+    while ( number != 0)
+    {
+    summary += number % 10;
+    number = number / 10;
+    }
+    return summary;
+ }
+
+ long summ = Summ(num);
+
+    Console.WriteLine($"Сумма чисел данного числа равна : {summ}");
