f0eaba337185973d7d4c0e8341d2242f2d3dcde8
lost1k <only.lost1k@gmail.com>
2022-11-10 03:57:43 +0300

    add HT29

--- HomeTask29/Program.cs
+++ HomeTask29/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая 
+// 1. задаёт массив из 8 элементов, заполненный псевдослучайными числами и 
+// 2. выводит их на экран.
+
+Console.WriteLine("Input size of array");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] AddArray(int amount)
+{
+    int[] array = new int[amount];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-100, 101);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ",");
+    }
+    Console.Write(array[num-1]);
+    Console.Write("]");
+}
+
+int[] addArray = AddArray(num);
+PrintArray(addArray);
