6cba681e32ff5cac22aaa6d74cdbac09dd87b8c8
lost1k <only.lost1k@gmail.com>
2022-11-13 02:26:48 +0300

    add task 35 37

---
+++ Task35/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 35: 
+// 1. Задайте одномерный массив из 123 случайных чисел.
+// 2. Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+// Console.WriteLine("Введите размер массива");
+// int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное число в массиве");
+int minElement = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное число в массиве");
+int maxElement = Convert.ToInt32(Console.ReadLine());
+
+int[] AddArrayRnd(int amount, int min, int max)
+{
+    int[] array = new int[amount];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) System.Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int Amount(int[] array)
+{
+    int count = 0;
+    int amount = 0;
+    while (count < array.Length)
+    {
+        if (array[count] > 9 && array[count] < 100)
+        {
+            amount++;
+            count++;
+        }
+        else
+        count ++;
+    }
+    return amount;
+}
+
+int[] rndArray = AddArrayRnd(123, minElement, maxElement);
+PrintArray(rndArray);
+int amount = Amount(rndArray);
+
+Console.WriteLine($"Количество элементов от 10 до 99 в данном массиве => {amount}");
