99226763248809742d20dcfddf90cc8c215e3bbb
lost1k <only.lost1k@gmail.com>
2022-11-19 02:34:34 +0300

    add 49&&51

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 49: 
+// 1. Задайте двумерный массив. 
+// 2. Найдите элементы, у которых оба индекса чётные, 
+// 3. и замените эти элементы на их квадраты.
+// Например, изначально массив 
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Новый массив будет выглядеть
+// вот так:
+// 1 4 49 2
+// 5 81 2 9
+// 64 4 4 4
+
+Console.WriteLine("Введите количество строк");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int col = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] CreateMatrixRndArray(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],5}|");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] SqrtMatrixEvenElements(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix2D = CreateMatrixRndArray(row, col, -9, 9);
+// PrintMatrix(matrix2D); Проверка корректного заполнения массива
+// Console.WriteLine("");
+matrix2D = SqrtMatrixEvenElements(matrix2D);
--- Task51/Program.cs
+++ Task51/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+int[,] CreateMatrixRndArray(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],5}|");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int DiagonalElementsSumm(int[,] matrix)
+{
+    int summ = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+                summ += matrix[i, j];
+        }
+    }
+    return summ;
+}
+
+Console.WriteLine("Введите количество строк");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное число в массиве (от -99999 до 99999)");
+int minElement = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное число в массиве (от -99999 до 99999)");
+int maxElement = Convert.ToInt32(Console.ReadLine());
+
+if (minElement >= -99999 && minElement <= 99999 && maxElement >= -99999 && maxElement <= 99999 && minElement <= maxElement)
+{
+    int[,] matrix2D = CreateMatrixRndArray(row, col, minElement, maxElement);
+    int summ = DiagonalElementsSumm(matrix2D);
+
+    Console.WriteLine("Сумма элементов, находящихся на главной диагонали массива");
+    PrintMatrix(matrix2D);
+    Console.WriteLine($"равна : {summ}");
+}
+else Console.WriteLine("Введите корректные значения!");
