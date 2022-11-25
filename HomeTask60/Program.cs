3ec2ad5f6886836d3a060f83c17c7071885ebed0
lost1k <only.lost1k@gmail.com>
2022-11-25 22:20:49 +0300

    add ht60

--- HomeTask60/Program.cs
+++ HomeTask60/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 60. ...
+// 1. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// 2. Напишите программу, которая будет построчно выводить массив, 
+// 3. добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] Create3dMatrixRndArray(int rows, int columns, int depth)
+{
+    int[,,] matrix = new int[rows, columns, depth];
+    // int rnd = 10;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Random rnd = new Random();
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rnd.Next(10, 100);
+                // matrix[i, j, k] = rnd;
+                // rnd+=1;
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("|");
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (k < matrix.GetLength(2) - 1)
+                    Console.Write($"{matrix[i, j, k],5}({i},{j},{k})|");
+                else Console.Write($"{matrix[i, j, k],5}({i},{j},{k})");
+            }
+            Console.WriteLine("|");
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите глубину");
+int depth = Convert.ToInt32(Console.ReadLine());
+
+int[,,] matrix3D = Create3dMatrixRndArray(row, col, depth);
+Console.WriteLine("Массив, заполненный случайными целыми числами");
+Console.WriteLine();
+PrintMatrix(matrix3D);
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,17 +1,48 @@
-﻿string[,] CreateArithmeticMeanArray(double[] array)
+﻿void PrintArray(int[] array)
 {
-    string[,] matrixNew = new string[4,array.Length];
-    int arrLength = 0;
-    int i = 0;
-    int j = 0;
-        for (j = 0; j < array.Length; j++)
-        {   
-            for (i = 0; i < array.Length; i++)
-            {
-                matrixNew[i, j] = Convert.ToString(array[arrLength]);
-            }
-            matrixNew[i, j] = $"Столбец {j}";
-            arrLength++;
-        }
-    return matrixNew;
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) System.Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
 }
+int[] AddArrayRnd(int amount)
+{
+    int[] array = new int[amount];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(10, 100);
+        for (int j = 1; j < array.Length; j++)
+        {
+            if (array[i] == array[j])
+            {
+                array[i] += 1;
+            }
+        }
+    }
+    return array;
+}
+
+int[] SortArrayForUniqNums(int[] array)
+{
+    int length = array.Length;
+    int[] arrayNew = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arrayNew[i] = array[i];
+        for (int j = 0; j < length; j++)
+        {
+            
+        }
+    }
+    return arrayNew;
+}
+
+int[] arrayNew = AddArrayRnd(20);
+int[] arrayNew2 = SortArrayForUniqNums(arrayNew);
+PrintArray(arrayNew);
+PrintArray(arrayNew2);
